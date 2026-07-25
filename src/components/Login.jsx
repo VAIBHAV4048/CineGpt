dcdbfb3912688c1491b198b29d4428581470d325
Vaibhav Pant <vpant4048@gmail.com>
2026-07-25 23:24:19 +0530

    feat: set up Firebase Hosting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from "react";
 import Header from "./Header";
 import { isValid } from "../utils/validate";
+import { auth } from "../utils/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
   const [sign, setSign] = useState(true);
@@ -11,6 +13,22 @@ const Login = () => {
   const signToggle = () => {
     setSign(!sign);
   };
+  if (showError) return;
+  if (!sign) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorcode + "-" + errorMessage);
+      });
+  }
 
   return (
     <>
