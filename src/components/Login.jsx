4c7cab9f58e769e78718c9e107cbd7c42c7961c6
Vaibhav Pant <vpant4048@gmail.com>
2026-07-25 23:28:31 +0530

    fix:fixed a small bug

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         const errorCode = error.code;
         const errorMessage = error.message;
         // ..
-        console.log(errorcode + "-" + errorMessage);
+        console.log(errorCode + "-" + errorMessage);
       });
   }
 
