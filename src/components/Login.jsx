61f2248552e9fdc70b4bc7f833ca96baefad755f
Vaibhav Pant <vpant4048@gmail.com>
2026-07-25 23:53:16 +0530

    feat: add error feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
           const errorCode = error.code;
           const errorMessage = error.message;
           // ..
-          console.log(errorCode + "-" + errorMessage);
+          setShowError(errorCode + "-" + errorMessage);
         });
     }
   };
