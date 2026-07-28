e93b554b748cf70f1093d459db94b3d59fa3cb32
Vaibhav Pant <vpant4048@gmail.com>
2026-07-28 16:49:46 +0530

    fix: fix routing flow-2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router'
-
+import Login from './components/Login.jsx'
+import Browse from './components/Browse.jsx'
 const appRouter = createBrowserRouter([
   {
     path: "/",
