2342005be7c61be7e744e2555a1daa23a1c84273
Vaibhav Pant <vpant4048@gmail.com>
2026-07-29 09:14:22 +0530

    feat: add routing to other page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, useNavigate } from "react-router";
 import { Outlet } from "react-router";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./utils/firebase";
+import { useDispatch } from "react-redux";
+
+import { addUser, removeUser } from "./utils/userSlice";
 
 function App() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+        navigate("/browse");
+        // ...
+      } else {
+        dispatch(removeUser());
+        navigate("/");// we can also use window.location.href
+        // User is signed out
+        // ...
+      }
+    });
+  }, []);
   return (
     <>
       <Header />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router'
-import Login from './components/Login.jsx'
-import Browse from './components/Browse.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import Login from "./components/Login.jsx";
+import Browse from "./components/Browse.jsx";
+
+
+
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -22,8 +25,8 @@ const appRouter = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <RouterProvider router={appRouter}></RouterProvider>
+    <RouterProvider router={appRouter}></RouterProvider>
   </StrictMode>,
-)
+);
