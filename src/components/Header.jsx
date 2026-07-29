1ceba6ef79904e83fea4d44e8bcb87a5000dcde2
Vaibhav Pant <vpant4048@gmail.com>
2026-07-29 17:44:13 +0530

    feat: build browse page-4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,37 @@
+import { Navigate, useNavigate } from "react-router";
 import logo from "../assets/logo.svg";
 import profile from "../assets/profile.svg";
+import { auth } from "../utils/firebase";
+
+
 const Header = ({ isSign }) => {
+  const navigate=useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error")
+      });
+  };
   return (
     <>
-      <header className={`w-full ${isSign ? "relative" : "absolute"} flex justify-between   z-10`}>
+      <header
+        className={`w-full ${isSign ? "relative" : "absolute"} flex justify-between   z-10`}
+      >
         <div className="logo m-4">
           <img className="w-24 lg:w-36" src={logo} alt="" />
         </div>
         {isSign && (
-          <div className="profile w-8 h-8 m-4">
-            <img src={profile} alt="profile" className="w-full h-full" />
+          <div className="profile w-8 h-8 m-4" onClick={handleSignOut}>
+            <img
+              src={profile}
+              alt="profile"
+              className="w-full h-full cursor-pointer"
+            />
           </div>
         )}
       </header>
