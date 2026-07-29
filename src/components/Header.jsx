9d0b2aeaccc2502396b8a6af624167bb178abad2
Vaibhav Pant <vpant4048@gmail.com>
2026-07-29 17:16:27 +0530

    feat: build browse page-3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import logo from "../assets/logo.svg";
+import profile from "../assets/profile.svg";
 const Header = ({ isSign }) => {
   return (
     <>
-      <header className={`w-full ${isSign ? "relative" : "absolute"}   z-10`}>
+      <header className={`w-full ${isSign ? "relative" : "absolute"} flex justify-between   z-10`}>
         <div className="logo m-4">
           <img className="w-24 lg:w-36" src={logo} alt="" />
         </div>
+        {isSign && (
+          <div className="profile w-8 h-8 m-4">
+            <img src={profile} alt="profile" className="w-full h-full" />
+          </div>
+        )}
       </header>
     </>
   );
