045ed55fb5f47fa55e0f7ee101e3f78eba8f4a06
Vaibhav Pant <vpant4048@gmail.com>
2026-07-29 23:51:27 +0530

    feat: build browse page-6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import logo from "../assets/logo.svg";
 import profile from "../assets/profile.svg";
 import { auth } from "../utils/firebase";
 import { signOut } from "firebase/auth";
-
+import { useState } from "react";
 const Header = ({ isSign }) => {
   const navigate = useNavigate();
+  const [popUp, setPopUp] = useState(false);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -26,7 +27,7 @@ const Header = ({ isSign }) => {
           <img className="w-24 lg:w-36" src={logo} alt="" />
         </div>
         {isSign && (
-          <div className="profile w-8 h-8 m-4" onClick={handleSignOut}>
+          <div className="profile w-8 h-8 m-4" onClick={setPopUp(true)}>
             <img
               src={profile}
               alt="profile"
@@ -34,6 +35,17 @@ const Header = ({ isSign }) => {
             />
           </div>
         )}
+
+        {popUp && (
+          <div className="absolute w-[90%] p-12 top-[50%] left-[50%] bg-amber-200 -translate-1/2 rounded-sm flex flex-col gap-6 bg-black/70 text-white max-w-md z-9">
+
+                  <button
+          className=" rounded-sm p-2  cursor-pointer w-full bg-[#A53DFC] hover:bg-[#9234df] text-lg font-semibold"
+          onClick={handleSignOut}
+        ></button>
+          </div>
+        )}
+   
       </header>
     </>
   );
