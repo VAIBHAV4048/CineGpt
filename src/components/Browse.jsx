e058e2fbc93a53a6b785924b354a68ccd9625582
Vaibhav Pant <vpant4048@gmail.com>
2026-08-07 00:28:51 +0530

    chore: test tmdb API-1

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,12 +1,20 @@
 import Header from "./Header";
 import { API_OPTION } from "../utils/constants";
+import { useEffect } from "react";
+
 const Browse = () => {
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
       API_OPTION,
     );
+    console.log(data.status);
     const json = await data.json();
+
     console.log(json);
   };
 
